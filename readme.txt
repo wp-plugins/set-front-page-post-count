57a54bbc7d4122ac16948cbbb1af14c730dfb585
Kawauso <Kawauso@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-10-02 13:59:29 +0000

    committing 0.1.1 to trunk
    git-svn-id: https://plugins.svn.wordpress.org/set-front-page-post-count/trunk@446403 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -1,9 +1,10 @@
 === Set Front Page Post Count ===
-Contributors: Kawauso
+Contributors: kawauso
+Donate link: http://adamharley.co.uk/buy-me-a-coffee/
 Tags: front page, posts, pre_get_posts
 Requires at least: 3.0
-Tested up to: 3.1
-Stable tag: 0.1
+Tested up to: 3.3
+Stable tag: 0.1.1
 
 Allows the front page to have a different number of posts than other pages.
 
@@ -11,12 +12,13 @@ Allows the front page to have a different number of posts than other pages.
 
 Adds a setting under Settings -> Reading to allow the front page post count to be controlled independently of the main post count and adjusts paging to accomodate for this.
 
-Uses the `pre_get_posts` hook and checks for the `$wp_query` query being passed, so may have issues with additional loops and `query_posts()`.
+Uses the `pre_get_posts` hook and checks for the `$wp_the_query` query being passed, so won't work with `query_posts()`.
 
 == Installation ==
 
 1. Upload `set-front-page-post-count.php` to the `/wp-content/plugins/` directory
 1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Set the front page post count in the 'Reading' section of the 'Settings' menu
 
 == Frequently Asked Questions ==
 
@@ -28,5 +30,14 @@ Due to the way that the WordPress query system is structured, the plugin may not
 
 == Changelog ==
 
+= 0.1.1 =
+* Adds missing default for when no front page count is set
+* Avoids applying query filter in admin
+
 = 0.1 =
 * First public release
+
+== Upgrade Notice ==
+
+= 0.1.1 =
+Missing default value added. Query filter no longer applied in admin.
--- set-front-page-post-count.php
+++ set-front-page-post-count.php
@@ -2,8 +2,10 @@
 /*
 Plugin Name: Set Front Page Post Count
 Description: Allows the front page to have a different number of posts than other pages.
-Author: Kawauso
-Version: 0.1
+Author: Adam Harley
+Version: 0.1.1
+Author URI: http://adamharley.co.uk
+Plugin URI: http://adamharley.co.uk/wordpress-plugins/set-front-page-post-count/
 */
 
 function sfppc_posts_settings_add() {
@@ -15,14 +17,14 @@ function sfppc_posts_settings_add() {
 function sfppc_posts_settings_form() {
 ?>
 
-<input name="front_page_post_count" type="text" id="front_page_post_count" value="<?php echo esc_attr( get_option( 'front_page_post_count', get_option('posts_per_page') ) ) ?>" class="small-text" /> <?php _e( 'posts', 'sfppc' ) ?>
+<input name="front_page_post_count" type="text" id="front_page_post_count" value="<?php echo esc_attr( get_option( 'front_page_post_count', get_option( 'posts_per_page' ) ) ) ?>" class="small-text" /> <?php _e( 'posts', 'sfppc' ) ?>
 
 <?php
 }
 
 
 function sfppc_posts_settings_sanitize( $value ) {
-	return is_numeric( $value ) ? $value : get_option( 'front_page_post_count', get_option('posts_per_page') );
+	return is_numeric( $value ) ? $value : get_option( 'front_page_post_count', get_option( 'posts_per_page' ) );
 }
 
 
@@ -30,15 +32,15 @@ function sfppc_posts_filter( &$query ) {
 	if ( $query != $GLOBALS['wp_the_query'] || !$query->is_home )
 		return;
 
-	if ( $query->is_paged ) {
-		$posts_per_page = $query->get('posts_per_page') ? $query->get('posts_per_page') : get_option('posts_per_page');
-		$offset = ($query->get('paged') - 2) * $posts_per_page + get_option( 'front_page_post_count', $posts_per_page );
-		$query->set( 'offset', $offset );
-	}
+	$posts_per_page = $query->get('posts_per_page') ? $query->get('posts_per_page') : get_option('posts_per_page');
+
+	if ( $query->is_paged ) 
+		$query->set( 'offset', ($query->get('paged') - 2) * $posts_per_page + get_option( 'front_page_post_count', $posts_per_page ) );
 	else
 		$query->set( 'posts_per_page', get_option( 'front_page_post_count', $posts_per_page ) );
 }
 
-
-add_action( 'admin_init', 'sfppc_posts_settings_add' );
-add_action( 'pre_get_posts', 'sfppc_posts_filter' );
+if ( is_admin() )
+	add_action( 'admin_init', 'sfppc_posts_settings_add' );
+else
+	add_action( 'pre_get_posts', 'sfppc_posts_filter' );
